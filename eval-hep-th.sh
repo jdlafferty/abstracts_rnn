4abaf37caf30a436559a549d319a3ed18b9377d0
jdlafferty <lafferty@gmail.com>
2019-03-17 14:44:34 +0000

    changed eval

--- eval-hep-th.sh
+++ eval-hep-th.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python ./evaluate.py --dataset "hep-th" \
+CUDA_VISIBLE_DEVICES=1 python ./evaluate.py --dataset "hep-th" --modelname "hep-th" \
     |& tee -a log_hep_th_eval.txt
