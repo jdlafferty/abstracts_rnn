Fixed likelihood computation and added script to estimate the likelihood of a string using all four models

rnns.py
@@ -303,23 +303,33 @@ class RNNs:
 
     batch_size = 1
     log_likelihood=0;
-    for i in range(0,len(test_seq) -1):
+
+    X = np.reshape(np.array([test_seq[0]]), [1, -1])
+    feed_dict={self._X: X,
+              self.init_state_place: np.zeros([self.n_layers, 2, batch_size, self.n_hidden]),
+              self.pkeep_placeholder: 1.0,
+              #self._seq_len: [i+1],
+              self._seq_len: [1],
+              self.batch_size: 1}
+    state, p_y_i = sess.run([self.final_state, self.p_y_i], feed_dict=feed_dict)
+    for i in range(1,len(test_seq) -1):
         X = np.reshape(np.array([test_seq[i]]), [1, -1])
         # assuming plainrnn
         feed_dict={self._X: X,
-                  self.init_state_place: np.zeros([self.n_layers, 2, batch_size, self.n_hidden]),
+                  self.init_state_place: state,
                   self.pkeep_placeholder: 1.0,
-                  self._seq_len: [i],
+                  #self._seq_len: [i+1],
+                  self._seq_len: [1],
                   self.batch_size: 1}
         state, p_y_i = sess.run([self.final_state, self.p_y_i], feed_dict=feed_dict)
-        log_likelihood = log_likelihood + np.log(p_y_i.reshape([-1])[test_seq[i+1]])
+        #pdb.set_trace()
+        log_likelihood = log_likelihood + np.log(p_y_i.reshape([-1])[test_seq[i+1]-1])
     return(log_likelihood)
 
 
   def generate_eq(self, sess):
     print("generating eqs...")
 
-    #pdb.set_trace()
     seq_start_list = ["<sos>"]*100 # ["<sos> \\", "<sos> {", "<sos> P"]
     for seq_start in seq_start_list:
       batch_size = 1
@@ -334,6 +344,7 @@ class RNNs:
                  self._seq_len: [len(generated_seq)],
                  self.batch_size: 1}
 
+      pdb.set_trace()
       state = sess.run(self.final_state, feed_dict=feed_dict)
 
       ### generate seq ###

train.py
@@ -14,10 +14,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--model_load_only", type=int, default=False)
 parser.add_argument("--dataset", type=str, default="")
 parser.add_argument("--modelname", type=str, default="")
+parser.add_argument("--load_training_data", type=str, default=True)
 args = parser.parse_args()
 
 def main(_):
-  config = Config(args.dataset, args.modelname)
+  config = Config(args.dataset, args.modelname,args.load_training_data)
   reader = Reader(config)
 
   with tf.Session() as sess:
@@ -31,6 +32,7 @@ def main(_):
 
     #generate text...
     if config.generate_text:
+      model.generate_eq(model.sess)
       test_string0 = "We trained a neural network."
       test_string1 = "Fofo fofo fd ss dd ddfffsss."
       test_string2 = "The meadow is very beautiful"
@@ -39,7 +41,6 @@ def main(_):
       print(model.compute_likelihood(model.sess, test_string1))
       print(model.compute_likelihood(model.sess, test_string2))
       print(model.compute_likelihood(model.sess, test_string3))
-      #model.generate_eq(model.sess)
 
 if __name__ == '__main__':
 	tf.app.run()